10b69e8cf36173d95aac499b59b8f374d580385d
Felix <felantezb@gmail.com>
2024-11-14 13:56:55 +0300

    new design on contact and portfolio

--- index.js
+++ index.js
@@ -77,7 +77,80 @@ ScrollReveal().reveal(
     origin: "bottom",
   }
 );
-ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h1, .about-img img, .heading2", {
+  origin: "left",
+});
 ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", {
   origin: "right",
 });
+/*ScrollReveal().reveal(".resume-heading", {
+  reset: true,
+  origin: "top",
+});*/
+
+/*-------------resume-------------------*/
+const resumeBtns = document.querySelectorAll(".resume-btn");
+resumeBtns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-detail");
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
+  });
+});
+
+/*-------carousel slide-----------*/
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+let index = 0;
+const maxIndex = 5;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+  if (index === 0) {
+    arrowLeft.classList.add("disabled");
+  } else {
+    arrowLeft.classList.remove("disabled");
+  }
+
+  if (index === maxIndex) {
+    arrowRight.classList.add("disabled");
+  } else {
+    arrowRight.classList.remove("disabled");
+  }
+};
+// Right arrow event
+arrowRight.addEventListener("click", () => {
+  if (index < maxIndex) {
+    index++;
+    activePortfolio();
+  }
+});
+
+// Left arrow event
+arrowLeft.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+    activePortfolio();
+  }
+});
+activePortfolio();
